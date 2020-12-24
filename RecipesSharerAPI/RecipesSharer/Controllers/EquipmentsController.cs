f49838294cc22b2d5e7f792e2fe11a06aeac3533
LeoRoma <xiajtn@gmail.com>
2020-12-24 04:10:32 +0000

    Using updated in Steps and Equipments Controllers

--- RecipesSharerAPI/RecipesSharer/Controllers/EquipmentsController.cs
+++ RecipesSharerAPI/RecipesSharer/Controllers/EquipmentsController.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using RecipesSharer.Models;
 
 namespace RecipesSharer.Controllers
 {
