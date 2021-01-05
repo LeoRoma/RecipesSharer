75d88b41d1993152e8977fe876eb5a3a3deff93c
LeoRoma <xiajtn@gmail.com>
2021-01-05 19:15:03 +0000

    DeleteImage added in ImageController

--- RecipesSharerAPI/RecipesSharer/Controllers/ImageController.cs
+++ RecipesSharerAPI/RecipesSharer/Controllers/ImageController.cs
@@ -7,6 +7,8 @@ using RecipesSharer.Models;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace RecipesSharer.Controllers
 {
@@ -14,27 +16,51 @@ namespace RecipesSharer.Controllers
     [ApiController]
     public class ImageController : Controller
     {
+        private readonly IConfiguration _config;
+        private readonly RecipesSharerDbContext _context;
+
+        public ImageController(RecipesSharerDbContext context, IConfiguration config)
+        {
+            _config = config;
+            _context = context;
+        }
+
         [Authorize]
         [HttpPost("post/{recipeId}")]
-        public async Task<Guid> Image(IFormFile image, int recipeId)
+        public async Task<Guid> PostImage(IFormFile image, int recipeId)
         {
-            using (var db = new RecipesSharerDbContext())
-            {
-                using (var ms = new MemoryStream())
-                {
-                    image.CopyTo(ms);
-                    var img = new Image()
-                    {
-                        Suffix = Path.GetExtension(image.FileName),
-                        Data = ms.ToArray()
-                    };
 
-                    img.RecipeId = recipeId;
-                    db.Images.Add(img);
-                    await db.SaveChangesAsync();
-                    return img.Id;
-                }
+            using (var ms = new MemoryStream())
+            {
+                image.CopyTo(ms);
+                var img = new Image()
+                {
+                    Suffix = Path.GetExtension(image.FileName),
+                    Data = ms.ToArray()
+                };
+
+                img.RecipeId = recipeId;
+                _context.Images.Add(img);
+                await _context.SaveChangesAsync();
+                return img.Id;
             }
         }
+
+        
+        [HttpDelete("delete/{id}")]
+        public async Task<ActionResult<Image>> DeleteImage(int id)
+        {
+            var image = await _context.Images.FirstOrDefaultAsync(i => i.RecipeId == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            _context.Images.Remove(image);
+            await _context.SaveChangesAsync();
+
+            return image;
+        }
+
     }
 }
